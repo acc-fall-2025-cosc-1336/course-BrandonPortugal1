d66ce0fe22458217ee07f972502e8097ccc4065c
BrandonPortugal1 <brandon.portugal@g.austincc.edu>
2025-09-14 02:35:47 +0000

    Competed HW01 assignment

--- src/homework/b_in_proc_out/output.py
+++ src/homework/b_in_proc_out/output.py
@@ -1,2 +1,6 @@
-def get_number(num):
+def get_number(num: int) -> int:
     return num
+
+def multiply_numbers(a: int, b: int) -> int:
+    return a * b
+
--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -16,3 +16,18 @@ class Test_Config(unittest.TestCase):
         #test that the function get_number returns 2
         self.assertEqual(2, get_number(2))
 
+import unittest
+from src.homework.b_in_proc_out.output import get_number, multiply_numbers
+
+class TestMultiplyNumbers(unittest.TestCase):
+    def test_multiply_positive(self):
+        self.assertEqual(multiply_numbers(get_number(7), get_number(7)), 49)
+        self.assertEqual(multiply_numbers(get_number(5), get_number(5)), 25)
+
+    def test_get_number(self):
+        self.assertEqual(get_number(10), 10)
+        self.assertEqual(get_number(-3), -3)
+
+if __name__ == '__main__':
+    unittest.main()
+
