26aa5526da3c0e8a23f785628c1d58cf99c548f5
BrandonPortugal1 <brandon.portugal@g.austincc.edu>
2025-10-04 22:25:46 +0000

    HW04

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,1 +1,46 @@
-#
+from src.homework.d_repetition.repetition import get_factorial, sum_odd_numbers
+
+def main():
+    while True:
+        print("\nHomework 4 Menu")
+        print("1 - Factorial")
+        print("2 - Sum odd numbers")
+        print("3 - Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            while True:
+                try:
+                    num = int(input("Enter a number (1-9): "))
+                    if 1 <= num <= 9:
+                        break
+                    else:
+                        print("Number must be between 1 and 9.")
+                except ValueError:
+                    print("Invalid input. Please enter an integer.")
+
+            print(f"Factorial of {num} is {get_factorial(num)}")
+
+        elif choice == "2":
+            while True:
+                try:
+                    num = int(input("Enter a number (1-99): "))
+                    if 1 <= num <= 99:
+                        break
+                    else:
+                        print("Number must be between 1 and 99.")
+                except ValueError:
+                    print("Invalid input. Please enter an integer.")
+
+            print(f"Sum of odd numbers up to {num} is {sum_odd_numbers(num)}")
+
+        elif choice == "3":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option. Try again.")
+
+if __name__ == "__main__":
+    main()
--- src/homework/d_repetition/repetition.py
+++ src/homework/d_repetition/repetition.py
@@ -1,1 +1,14 @@
-#
+def get_factorial(number):
+    result = 1
+    for i in range(1, number + 1):
+        result *= i
+    return result
+
+def sum_odd_numbers(number):
+    total = 0
+    i = 1
+    while i <= number:
+        if i % 2 != 0:
+            total += i
+        i += 1
+    return total
